dee9c98105177d9db1593ee4d2efbe06e0f47160
Jakha Cham <you@example.com>
2026-06-24 23:03:58 -0400

    Testing

--- game/actions.py
+++ game/actions.py
@@ -6,24 +6,68 @@ from database.db import get_plant, log_action
 from game.plant_growth import update_plant_progression
 
 
+# def perform_action(user_id, action_type, distance=0, city="Atlanta"):
+
+#     if action_type in ["bike", "walk", "bus"]:
+
+#         saved = carbon_saved(action_type, distance)
+
+#         if saved is None:
+#             saved = 1.0
+
+#     else:
+#         saved = 0.5
+
+#     log_action(
+#         user_id,
+#         action_type,
+#         saved
+#     )
+
+#     weather_bonus = int(saved) 
+
+#     update_plant_progression(
+#         user_id,
+#         weather_bonus,
+#         0
+#     )
+
+#     plant = get_plant(user_id)
+
+#     if plant is None:
+#         return "No plant found for this user."
+
+#     weather = get_weather(city)
+
+#     weather_desc = "Unknown"
+
+#     if weather:
+#         weather_desc = weather["description"]
+
+#     feedback = sprout_feedback(
+#         action_type,
+#         saved,
+#         weather_desc,
+#         plant_stage = plant["stage"] if plant else "Seed"
+#     )
+
+#     return feedback
 def perform_action(user_id, action_type, distance=0, city="Atlanta"):
 
     if action_type in ["bike", "walk", "bus"]:
-
         saved = carbon_saved(action_type, distance)
-
         if saved is None:
             saved = 1.0
-
     else:
         saved = 0.5
 
-    log_action(
-        user_id,
-        action_type,
-        saved
-    )
+    log_action(user_id, action_type, saved)
 
+    weather_bonus = int(saved)
+
+    plant = get_plant(user_id)
+    if plant is None:
+        return "No plant found for this user."
 
     update_plant_progression(
         user_id,
@@ -31,23 +75,14 @@ def perform_action(user_id, action_type, distance=0, city="Atlanta"):
         0
     )
 
-    plant = get_plant(user_id)
-
-    if plant is None:
-        return "No plant found for this user."
-
     weather = get_weather(city)
-
-    weather_desc = "Unknown"
-
-    if weather:
-        weather_desc = weather["description"]
+    weather_desc = weather["description"] if weather else "Unknown"
 
     feedback = sprout_feedback(
         action_type,
         saved,
-        weather_desc,
-        plant_stage = plant["stage"] if plant else "Seed"
+        plant["stage"],
+        weather_desc
     )
 
     return feedback
